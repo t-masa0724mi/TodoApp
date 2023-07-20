7175d6f9a5420f390feba513be8d502d81d0ba23
masa950724mi <s100.iizuka.masami@gmail.com>
2023-07-20 14:11:10 +0900

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by 飯塚政美 on 2023/07/20.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
