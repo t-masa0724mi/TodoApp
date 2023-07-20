3ea16b8785e71c8f1478c41e6542e57242792ce8
masa950724mi <s100.iizuka.masami@gmail.com>
2023-07-20 14:12:25 +0900

    first commit

--- TodoApp/ContentView.swift
+++ TodoApp/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  TodoApp
-//
-//  Created by 飯塚政美 on 2023/07/20.
-//
-
 import SwiftUI
 
 struct ContentView: View {
--- TodoApp/TodoAppApp.swift
+++ TodoApp/TodoAppApp.swift
@@ -1,10 +1,3 @@
-//
-//  TodoAppApp.swift
-//  TodoApp
-//
-//  Created by 飯塚政美 on 2023/07/20.
-//
-
 import SwiftUI
 
 @main
